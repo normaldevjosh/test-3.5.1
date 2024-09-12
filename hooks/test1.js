f80cc28a0db4d108a4536391d09d3944b2e51f8f
normaljosh <josh@normal.dev>
2024-09-12 14:26:37 -0400

    update stuff

--- hooks/test1.js
+++ hooks/test1.js
@@ -7,7 +7,7 @@ const cowsay = require("cowsay");
  * @returns {NormalSdk.InvokeResult}
  */
 module.exports = async ({ points, sdk, update, args, config }) => {
-  console.log("test1; version 3");
+  console.log("test1; version 4;this should run every 30 seconds.");
 
   cconsole.log(
     cowsay.say({
