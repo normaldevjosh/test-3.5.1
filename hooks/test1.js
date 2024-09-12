d4a6472e26ca99bb8f0419cb8fd232e2a11b7584
normaljosh <josh@normal.dev>
2024-09-12 14:15:37 -0400

    update version

--- hooks/test1.js
+++ hooks/test1.js
@@ -6,7 +6,7 @@ const NormalSdk = require("@normalframework/applications-sdk");
  * @returns {NormalSdk.InvokeResult}
  */
 module.exports = async ({ points, sdk, update, args, config }) => {
-  console.log("test1; version 1");
+  console.log("test1; version 2");
 
   console.log(JSON.stringify(config));
 };
